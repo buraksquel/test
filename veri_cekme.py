73420655cc69d5ecf0b024480b9e7a1422fb8e1b
buraksquel <buraksoylu2101@gmail.com>
2024-03-01 00:16:07 +0300

    ikinci commit

--- veri_cekme.py
+++ veri_cekme.py
@@ -8,6 +8,7 @@ def dünya_kısa_acıklama_cek(url):
     # Sayfadaki ilk <p> etiketini bul ve metnini alıyorum
     p_etiketi = soup.find("p")
     return p_etiketi.text if p_etiketi else "Kısa açıklama bulunamadı."
+    # test
 
 def google_haber_dünya_haber_cekici(Dünya):
     # Google Haberler'in Dünya kategorisine giderek sayfa içeriğini çekiyorum
